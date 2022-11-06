b0d9a0a19af5f37f9b4ed2e3ce94d0a1af3870a1
rodo08 <rodrigo.rosalesmoya@gmail.com>
2022-11-06 13:08:34 +0100

    Add not-found test and next test

--- src/server/controllers/robotController.test.ts
+++ src/server/controllers/robotController.test.ts
@@ -1,7 +1,13 @@
-import type { Response } from "express";
+import type { Response, NextFunction } from "express";
 import Robot from "../../database/models/Robot";
 import roboMock from "../../mocks/mocks";
 import { getRobots } from "./robotsController";
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+const next = jest.fn();
 
 const res: Partial<Response> = {
   json: jest.fn(),
@@ -10,7 +16,7 @@ const res: Partial<Response> = {
 
 describe("Given a getRobots controller", () => {
   describe("When it finds a list of robots", () => {
-    test("Then it should call athe 200 response method status and a json method", async () => {
+    test("Then it should call the 200 response method status and a json method", async () => {
       const expectedStatus = 200;
 
       Robot.find = jest.fn().mockReturnValue(roboMock);
@@ -19,6 +25,28 @@ describe("Given a getRobots controller", () => {
 
       expect(res.status).toHaveBeenCalledWith(expectedStatus);
       expect(res.json).toHaveBeenCalled();
+    });
+  });
+
+  describe("When it receives an empty array", () => {
+    test("Then it should call the 404 response method", async () => {
+      const expectedStatus = 404;
+
+      Robot.find = jest.fn().mockReturnValue([]);
+
+      await getRobots(null, res as Response, null);
+
+      expect(res.status).toHaveBeenCalledWith(expectedStatus);
+    });
+  });
+
+  describe("When it receives a response with an error", () => {
+    test("Then next should be called", async () => {
+      Robot.find = jest.fn().mockRejectedValue(new Error(""));
+
+      await getRobots(null, res as Response, next as NextFunction);
+
+      expect(next).toHaveBeenCalled();
     });
   });
 });
